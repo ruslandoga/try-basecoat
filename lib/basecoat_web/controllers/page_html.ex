cb4394e922d35429439924ac0b196e245f819c4b
ruslandoga <ruslandoga+gh@icloud.com>
2025-05-18 22:35:21 +0300

    why is it all white

--- lib/basecoat_web/controllers/page_html.ex
+++ lib/basecoat_web/controllers/page_html.ex
@@ -10,10 +10,26 @@ defmodule BasecoatWeb.PageHTML do
     ~H"""
     <Layouts.flash_group flash={@flash} />
 
-    <div class="container mx-auto p-4">
+    <div class="container mx-auto p-4 space-y-4">
       <.component_preview name="Alert">
         <div class="max-w-md">
-          <.alert />
+          <.alert title="Success! Your changes have been saved">
+            This is an alert with icon, title and description.
+          </.alert>
+        </div>
+      </.component_preview>
+
+      <.component_preview name="Badge">
+        <.badge>New</.badge>
+      </.component_preview>
+
+      <.component_preview name="Button">
+        <.new_button>Button</.new_button>
+      </.component_preview>
+
+      <.component_preview name="Card">
+        <div class="max-w-sm">
+          <.card />
         </div>
       </.component_preview>
     </div>
@@ -45,8 +61,55 @@ defmodule BasecoatWeb.PageHTML do
       >
         <circle cx="12" cy="12" r="10" /><path d="m9 12 2 2 4-4" />
       </svg>
-      <h2>Success! Your changes have been saved</h2>
-      <section>This is an alert with icon, title and description.</section>
+      <h2>{@title}</h2>
+      <section>{render_slot(@inner_block)}</section>
+    </div>
+    """
+  end
+
+  defp badge(assigns) do
+    ~H"""
+    <span class="badge">{render_slot(@inner_block)}</span>
+    """
+  end
+
+  defp new_button(assigns) do
+    ~H"""
+    <button class="btn">{render_slot(@inner_block)}</button>
+    """
+  end
+
+  def card(assigns) do
+    ~H"""
+    <div class="card w-full">
+      <header>
+        <h2>Login to your account</h2>
+        <p>Enter your details below to login to your account</p>
+      </header>
+      <section>
+        <form class="form grid gap-6">
+          <div class="grid gap-2">
+            <label for="demo-card-form-email">Email</label>
+            <input type="email" id="demo-card-form-email" />
+          </div>
+          <div class="grid gap-2">
+            <div class="flex items-center gap-2">
+              <label for="demo-card-form-password">Password</label>
+              <a href="#" class="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                Forgot your password?
+              </a>
+            </div>
+            <input type="password" id="demo-card-form-password" />
+          </div>
+        </form>
+      </section>
+      <footer class="flex flex-col items-center gap-2">
+        <button type="button" class="btn w-full">Login</button>
+        <button type="button" class="btn-outline w-full">Login with Google</button>
+        <p class="mt-4 text-center text-sm">
+          Don't have an account? <a href="#" class="underline-offset-4 hover:underline">Sign up</a>
+        </p>
+      </footer>
     </div>
     """
   end
